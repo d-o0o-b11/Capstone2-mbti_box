bb38fc8da94c6b73416a21e41b99770d9897454f
d-o0o-b11 <97296843+d-o0o-b11@users.noreply.github.com>
2022-05-01 00:11:19 +0900

    write

--- mbti_box/src/write/writeINTJ.js
+++ mbti_box/src/write/writeINTJ.js
@@ -1,17 +1,137 @@
 import React, { useState } from "react";
 import "../write/writes.css"
-import styled from "styled-components"
-import {CKEditor} from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import Parser from 'html-react-parser';
 
 
+const WRITEINTJ = (props) => {
+  const [post,setPost] = useState({
+    title:'',
+    desc:'',
+    photos:[]
+  });
 
-const WRITEINTJ = () => {
-  
+  const [highlight, setHighlight]= useState(false);
+  const {title, desc, photos}=post;
+  const handlechange = e =>{
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handlefilechange = e =>{
+    let files = e.target.files;
+    handfiles(files);
+    //console.log(files);
+    // let photosArr =[];
+    // for(let file of files){
+    //   let reader = new FileReader();
+    //   reader.readAsDataURL(file);
+    //   reader.addEventListener('load',()=>{
+    //     let fileobj={
+    //       name: file.name,
+    //       type: file.type,
+    //       size: file.size,
+    //       src: reader.result
+    //     }
+    //     photosArr.push(fileobj);
+    //     setPost({
+    //       ...post,
+    //       photos: [...photos, ...photosArr]
+    //     })
+    //   });
+    // }
+  }
+
+  const handfiles=files=>{
+    let photosArr =[];
+    for(let file of files){
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.addEventListener('load',()=>{
+        let fileobj={
+          name: file.name,
+          type: file.type,
+          size: file.size,
+          src: reader.result
+        }
+        photosArr.push(fileobj);
+        setPost({
+          ...post,
+          photos: [...photos, ...photosArr]
+        })
+      });
+    }
+  }
+
+
+  const handledelete=e=>{
+    let target = e.target.parentElement;
+    let targetindex = target.dataset.imgindex;
+    // console.log(target, targetindex);
+     setPost({
+       ...post,
+       photos: [...photos.slice(0,targetindex), ...photos.slice(targetindex+1)]
+     })
+  }
+
+  const handlehighlight=e=>{
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(true);
+  }
+
+  const handleunhiglight=e=>{
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(false);
+  }
+
+  const handledrop=e=>{
+    e.preventDefault();
+    e.stopPropagation();
+
+    let dt= e.dataTransfer;
+    let files= dt.files;
+    setHighlight(false);
+    handfiles(files);
+    console.log(files);
+  }
 
   return (
-    <></>
+        <div className="file-upload">
+        <h2>Image Drag & Drop & Preview</h2>
+        
+            <div className="custom-form-group" >
+                <input type="text" name="title" placeholder="Title" value={title} onChange={handlechange}/>
+            </div>
+            <div className="custom-form-group">
+                <input type="text" name="desc" placeholder="Description" value={desc} onChange={handlechange}/>
+            </div>
+            <div className="custom-form-group">
+                <div className={highlight? "custom-file-drop-area highlight": "custom-file-drop-area "} 
+                onDragEnter={handlehighlight} 
+                onDragOver={handlehighlight} 
+                onDragLeave={handleunhiglight}
+                onDrop={handledrop}
+                >
+                    <input type="file"name="photos" placeholder="Enter photos" multiple id="filephotos" onChange={handlechange}/>
+                    <label htmlFor="filephotos">Drag & Drop</label>
+                </div>
+                <div className="custom-file-preview">
+                  {photos.length > 0 && photos.map((item,index)=>(
+                    <div className="prev-img" key={index} data-imgindex={index}>
+                      <span onClick={handledelete}>&times;</span>
+                      <img src={item.src} alt={item.name}/>
+                    </div>
+                  ))}
+                    
+                </div>
+            </div>
+            <button type="submit" className="btn-submit">Submit</button>
+        
+    </div>
+
+
   );
 };
 
